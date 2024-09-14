817b37480fdc1c25074a2aa66ebb7c25f7c6c6a5
Mutmainah <mutmainahabike@gmail.com>
2024-09-14 14:54:51 +0100

    my first commit

---
+++ intro/data-type.js
@@ -0,0 +1,34 @@
+// variable
+
+var myName = 'Mutmainah';
+console.log(myName)
+
+//Number
+let age = 30;
+console.log(age)
+
+let address;
+// console.log(address)
+
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30
+};
+console.log(person)
+let color ={
+    white: "#ffffff",
+    black: "000000"
+}
+console.log(color)
+let basket ={
+    car: ["BMW", "Toyota", "Honda", "Benz", "Ford"],
+    color: ["red", "green", "blue"],
+    fruit: ["apple", "orange", "banana", "grape"]
+}
+console.log(basket)
+
+let x = BigInt(123456789012345);
+let y = BigInt(7777777)
+console.log(x*y)
+
---
+++ intro/variable.js
@@ -0,0 +1,9 @@
+// variable
+
+/*
+3 ways to declare a variable
+1. var: function scope
+2. let: block scope & global scope
+3: const: something that should never change
+
+var username */
